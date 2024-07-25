a7e25a02bf63aa73d6ee56497694062f8e2f1a4d
Ray Andrade <TheRayCode@RayAndrade.com>
2024-07-25 12:01:28 -0700

    *

---
+++ Creational/AbstractFactory/Example/src/CatFactory.java
@@ -0,0 +1,2 @@
+public class CatFactory {
+}
--- Creational/AbstractFactory/Example/src/Parrot.java
+++ Creational/AbstractFactory/Example/src/Parrot.java
@@ -1,2 +1,11 @@
-public class Parrot {
+public class Parrot implements Animal {
+    @Override
+    public String getSpecies() {
+        return "Parrot";
+    }
+
+    @Override
+    public String makeSound() {
+        return "Poly wanna cracker";
+    }
 }
